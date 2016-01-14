3178f007025220de5ae0a4cf20a82fb49353c2d7
Marko Lovic <markolovic@hotmail.com>
2016-01-14 17:30:29 +0100

    comment out test. Should be removed

--- spec/weekly_class_spec.rb
+++ spec/weekly_class_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe WeeklyClass do
-  it 'returns correct time' do
-    #allow(DateTime).to receive(:now) { DateTime.new(year: 
-    now = DateTime.now
-    wc = WeeklyClass.new(starts_at: Time.parse('17:00'), ends_at: Time.parse('18:00') + 60*60, day_of_week: now.wday)
-    expect(wc.abs_start_time).to eq now.change(hour: 17, minute: 0, second: 0).to_time
-  end
+  #it 'returns correct time' do
+    ##allow(DateTime).to receive(:now) { DateTime.new(year: 
+    #now = DateTime.now
+    #wc = WeeklyClass.new(starts_at: Time.parse('17:00'), ends_at: Time.parse('18:00') + 60*60, day_of_week: now.wday)
+    #expect(wc.abs_start_time).to eq now.change(hour: 17, minute: 0, second: 0).to_time
+  #end
 end
