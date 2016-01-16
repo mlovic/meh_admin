554fd5d0b899c128837b66aaa0767689241fa3df
Marko Lovic <markolovic@hotmail.com>
2016-01-16 12:07:45 +0100

    Add #hours_per_week to teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,11 @@
 class Teacher < ActiveRecord::Base
   has_many :groups
+  
+  validates :color, uniqueness: true
+  validates :name, uniqueness: true, presence: true
+
+  def hours_per_week
+    groups.map(&:hours_per_week).inject(:+)
+  end
+
 end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  let(:teacher) { build(:teacher) }
+  describe '#hours_per_weeek' do
+    it 'works' do
+      build(:group, teacher: teacher).add_biweekly_schedule(:monday, :wednesday, '19:00', 60*90)
+      build(:group, teacher: teacher).add_biweekly_schedule(:monday, :wednesday, '17:00')
+      expect(teacher.hours_per_week).to eq 5
+    end
+  end
 end
