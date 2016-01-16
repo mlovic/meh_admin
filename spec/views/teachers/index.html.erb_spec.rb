9b248457d8c76fab696a945f6e1b272c6c49ed24
Marko Lovic <markolovic@hotmail.com>
2016-01-16 12:05:33 +0100

    correct teachers view spec

--- spec/views/teachers/index.html.erb_spec.rb
+++ spec/views/teachers/index.html.erb_spec.rb
@@ -2,21 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "teachers/index", type: :view do
   before(:each) do
-    assign(:teachers, [
-      Teacher.create!(
-        :name => "Name",
-        :color => "Color"
-      ),
-      Teacher.create!(
-        :name => "Name",
-        :color => "Color"
-      )
-    ])
+    assign(:teachers, [create(:izzy), create(:stefan)])
   end
 
   it "renders a list of teachers" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Color".to_s, :count => 2
+    assert_select "tr>td", :text => "Izzy".to_s, :count => 1
+    assert_select "tr>td", :text => "Stefan".to_s, :count => 1
+    #assert_select "tr>td", :text => "red".to_s, :count => 2
   end
 end
